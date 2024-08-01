1449e482a50122ac0e8c3a67136773cc54e4cdf5
L7dri <ayoubeh98@gmail.com>
2024-08-01 19:43:37 +0100

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -6,13 +6,13 @@ class BankAccount:
   def deposit(self, amount):
     if amount > 0 :
       self.balance_account += amount 
-    print("Deposited:"; amount)
+    print("Deposited: $" amount)
     else ValueError :
     print(" Invalid amount ")
   def withdraw(self, amount):
     if amount <= self.balance_account and amount >0:
       self.balance_account -= amount 
-      print("Withdrew:" amount)
+      print("Withdrew: $" amount)
     else 
       print("Insufficient funds.")
   def display_balance(self):
